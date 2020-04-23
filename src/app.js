1ec39bbcaad1bd2f4721569b2a132eba9a7418e8
chenximan <15320015211@163.com>
2020-04-23 09:08:44 +0800

    使用 npm test 和 npm run dev-test 自动测试

--- src/app.js
+++ src/app.js
@@ -96,8 +96,8 @@ const expect = chai.expect
     let spy = chai.spy(function(){})
 
     vm.$on('click', spy)
-    //希望这个函数被执行
+    // 希望这个函数被执行
     let button = vm.$el
     button.click()
-    expect(spy).to.hav.been.called()
+    expect(spy).to.have.been.called()
 }
